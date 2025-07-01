f429787218e6cee6f282c9951770b1c326342640
Sebastian Mettes <smettes3@gatech.edu>
2025-07-01 16:08:18 +0100

    new images

--- vision_tools.py
+++ vision_tools.py
@@ -170,7 +170,7 @@ class VisionTools:
                 best_match = max_loc
                 best_angle = angle
         
-        if best_score > 0.6:
+        if best_score > 0.55:
             h, w = template.shape
             top_left = best_match
             bottom_right = (top_left[0] + w, top_left[1] + h)
