e491e8aa4b9620345edb595ed4e0f2656c0d905a
Sebastian Mettes <smettes3@gatech.edu>
2025-11-06 14:33:42 +0000

    updated templates for PIN diodes

--- pnp_operations.py
+++ pnp_operations.py
@@ -568,7 +568,7 @@ class PnPWorkflow:
 
 
         self.printer.linear_move(z=150,f=6000) #Lift to safe height
-        self.printer.linear_move(x=pickup_pos['X']-0.6, y=pickup_pos['Y']-0.5,f=6000) #Note - manual offsets
+        self.printer.linear_move(x=pickup_pos['X']-0.4, y=pickup_pos['Y']-0.25,f=6000) #Note - manual offsets
         self.printer.wait_for_idle()
         self.printer.control_solenoid(True)
         self.printer.control_vacuum(True)
