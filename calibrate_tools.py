32b32e8a105ca72c1bc1cca994e9cdcb75fbfee2
Sebastian Mettes <smettes3@gatech.edu>
2026-02-06 18:41:48 +0000

    updated camera configs, machine config, Schottky diode image

--- calibrate_tools.py
+++ calibrate_tools.py
@@ -25,8 +25,8 @@ from machine_vision import VisionTools, CameraConfig, load_camera_config
 from machine_control import Printer, center_target_in_camera
 
 # Configuration
-TARGET_PICKUP_LOCATION = [10.0, 10.0, 1.0]  # For Tool 3 calibration
-TARGET_PLACE_LOCATION = [110.0, 110.0, 2]  # For Tool 3 calibration
+TARGET_PICKUP_LOCATION = [10.0, 10.0, -2]  # For Tool 3 calibration
+TARGET_PLACE_LOCATION = [110.0, 110.0, 3]  # For Tool 3 calibration
 TOOL3_CHECK_HEIGHT = 158.0
 
 
